588830bc175a3a585435a036fb354853ac6125a4
dralv <dralvphd2@gmail.com>
2022-12-23 15:10:55 -0300

    Definindo o Layout e Consumindo o Microsserviço de Produtos

--- FrontEnd/GeekShopping.Web/Models/ProductModel.cs
+++ FrontEnd/GeekShopping.Web/Models/ProductModel.cs
@@ -4,6 +4,7 @@
     {
         public long Id { get; set; }
         public string Name { get; set; }
+
         public decimal Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
