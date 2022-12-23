f76c73da4152cfffd94678849dc3ad2242853dbb
dralv <dralvphd2@gmail.com>
2022-12-23 13:34:23 -0300

    Preparando Microsserviço Web para Consumir o Microsserviço de Produtos

--- FrontEnd/GeekShopping.Web/Program.cs
+++ FrontEnd/GeekShopping.Web/Program.cs
@@ -1,8 +1,13 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpClient<IProductService,ProductService>(c=>
+    c.BaseAddress = new Uri(builder.Configuration["ServicesUrls:ProductAPI"])
+    );
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
